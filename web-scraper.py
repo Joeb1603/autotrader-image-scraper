44253fe339256cc4e10b3d819f09d43554c4d88e
joeb1603 <joeb1603@gmail.com>
2024-01-22 16:45:25 +0000

    Gets first image from web page - issue with cloudflare on autotrader

--- web-scraper.py
+++ web-scraper.py
@@ -1,1 +1,24 @@
-print("hello")
+#pip install requestions
+#pip install bs4
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+import webbrowser
+
+page_url = "https://www.geeksforgeeks.org/"
+#"https://www.autotrader.co.uk/car-details/202310142996962?sort=relevance&body-type=Convertible&postcode=NE16an&advertising-location=at_cars&fromsra"
+
+page_data = requests.get(page_url) 
+
+soup = BeautifulSoup(page_data.content, "html.parser")
+#print(soup)
+
+images = soup.find_all('img')
+
+if len(images)>0:
+    first_image = images[0]['src']
+    print(first_image)
+    webbrowser.open(first_image)
+else:
+    print("No images found")
