23c8ead5aeb4578e997ee9a135cfc6ed227e710d
joeb1603 <joeb1603@gmail.com>
2024-01-23 00:33:20 +0000

    Will now go through all images on page and download them

--- web-scraper.py
+++ web-scraper.py
@@ -6,6 +6,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
 import webbrowser
+from PIL import Image
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -21,6 +22,7 @@ driver = webdriver.Chrome(options=options)
 
 print("\n\n\n")
 
+# Set stuff up
 mx5_1 = "https://www.autotrader.co.uk/car-details/202310142996962"
 mx5_2 = "https://www.autotrader.co.uk/car-details/202401125480291"
 
@@ -31,9 +33,10 @@ print(f"The title is: {title}")
 
 driver.implicitly_wait(0.5)
 
-
-cookie_frame_id = "sp_message_iframe_982392" #id
+# Accept the cookies
+cookie_frame_id = "sp_message_iframe_982392" 
 cookie_button_id = "/html/body/div/div[2]/div[4]/button[3]"
+
 
 cookie_frame = driver.find_element(By.ID, cookie_frame_id)
 
@@ -44,20 +47,39 @@ cookie_button = wait.until(EC.element_to_be_clickable((By.XPATH, cookie_button_i
 cookie_button.click()
 
 
+driver.implicitly_wait(0.5)
+
+# Click onto the firest image of the image gallery
 buttons_to_click = ['/html/body/div[1]/main/div/div[2]/article/section[1]/div/div/div[1]/button[2]',
-                    "/html/body/div[2]/div[2]/div/section/div[2]/div/div/div/div[2]/ul/li[1]/button/div/div/div/div/div/picture/div/img"]
+                        "/html/body/div[2]/div[2]/div/section/div[2]/div/div/div/div[2]/ul/li[1]/button/div/div/div/div/div/picture/div/img"]
 
 
 for button in buttons_to_click:
     driver.find_element(By.XPATH, button).click()
 
+driver.implicitly_wait(0.5)
 
+# Go and click through all the images and download them 
+images_parent_div_id = '/html/body/div[3]/div[2]/div/section/div[2]/div/div/div/div[2]/div/div/div'
 next_img_button_id = "/html/body/div[3]/div[2]/div/section/div[2]/div/div/div/div[2]/div/button[2]"
+
+from pathlib import Path
+Path("images").mkdir(parents=False, exist_ok=True)
+
+x=0
 while True:
+    x+=1
     try:
+        current_url = driver.find_element(By.XPATH, images_parent_div_id).find_element(By.CLASS_NAME, "slick-current").find_element(By.TAG_NAME, 'img').get_attribute("src")
+        
+        data = requests.get(current_url).content 
+        with open(f'images/{x}.png','wb') as handler:
+            handler.write(data) 
+
         driver.find_element(By.XPATH, next_img_button_id).click()
+        driver.implicitly_wait(0.5)
     except:
-        print("All images seen")
+        print("All images should be done")
         break
 
-
+print("\n\n\n")
