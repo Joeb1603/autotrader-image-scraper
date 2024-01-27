0feb12934491a5489867bcde663326d162655881
joeb1603 <joeb1603@gmail.com>
2024-01-27 15:39:30 +0000

    Added multiple ways of filtering out the false positives. Added a check to make sure the area of the car detection is large enough. Added check to make sure the detection is not on the edge of the image (filters out images where some of the car is off-screen, such as close ups of thwe wheels etc. Added code to remove the white borders from some images, which were causing the detection on the edge checker not always work. The false positive rate is now substantially lower, and the false negative rate has also been kept low.

--- car_detector.py
+++ car_detector.py
@@ -10,6 +10,7 @@ from numpy import random
 import shutil
 import warnings
 import os
+import numpy as np
 
 path_to_yolo = 'F:\Programming\Dissertation-mk2\yolov7'
 sys.path.insert(0, path_to_yolo)
@@ -25,6 +26,139 @@ from utils.torch_utils import select_device, load_classifier, time_synchronized,
 car_path = "contains-car" # probably shouldn't have these outside the functions but it works
 no_car_path = "no-car"
 
+import glob
+
+import glob
+import logging
+import os
+import random
+import time
+
+from copy import deepcopy
+#from pycocotools import mask as maskUtils
+from torchvision.utils import save_image
+from torchvision.ops import roi_pool, roi_align, ps_roi_pool, ps_roi_align
+
+
+# Parameters
+help_url = 'https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data'
+img_formats = ['bmp', 'jpg', 'jpeg', 'png', 'tif', 'tiff', 'dng', 'webp', 'mpo']  # acceptable image suffixes
+vid_formats = ['mov', 'avi', 'mp4', 'mpg', 'mpeg', 'm4v', 'wmv', 'mkv']  # acceptable video suffixes
+logger = logging.getLogger(__name__)
+
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+class NewLoadImages:  # Slightly modified dataloader from yolov7
+    def __init__(self, path, img_size=640, stride=32):
+        p = str(Path(path).absolute())  # os-agnostic absolute path
+        if '*' in p:
+            files = sorted(glob.glob(p, recursive=True))  # glob
+        elif os.path.isdir(p):
+            files = sorted(glob.glob(os.path.join(p, '*.*')))  # dir
+        elif os.path.isfile(p):
+            files = [p]  # files
+        else:
+            raise Exception(f'ERROR: {p} does not exist')
+
+        images = [x for x in files if x.split('.')[-1].lower() in img_formats]
+        videos = [x for x in files if x.split('.')[-1].lower() in vid_formats]
+        ni, nv = len(images), len(videos)
+
+        self.img_size = img_size
+        self.stride = stride
+        self.files = images + videos
+        self.nf = ni + nv  # number of files
+        self.video_flag = [False] * ni + [True] * nv
+        self.mode = 'image'
+        if any(videos):
+            self.new_video(videos[0])  # new video
+        else:
+            self.cap = None
+        assert self.nf > 0, f'No images or videos found in {p}. ' \
+                            f'Supported formats are:\nimages: {img_formats}\nvideos: {vid_formats}'
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        if self.count == self.nf:
+            raise StopIteration
+        path = self.files[self.count]
+
+        if self.video_flag[self.count]:
+            # Read video
+            self.mode = 'video'
+            ret_val, img0 = self.cap.read()
+            if not ret_val:
+                self.count += 1
+                self.cap.release()
+                if self.count == self.nf:  # last video
+                    raise StopIteration
+                else:
+                    path = self.files[self.count]
+                    self.new_video(path)
+                    ret_val, img0 = self.cap.read()
+
+            self.frame += 1
+            print(f'video {self.count + 1}/{self.nf} ({self.frame}/{self.nframes}) {path}: ', end='')
+
+        else:
+            # Read image
+            self.count += 1
+            img0 = cv2.imread(path)  # BGR
+            assert img0 is not None, 'Image Not Found ' + path
+            #print(f'image {self.count}/{self.nf} {path}: ', end='')
+
+        img0 = remove_borders(img0)
+
+        # Padded resize
+        img = letterbox(img0, self.img_size, stride=self.stride)[0]
+
+        # Convert
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+
+        return path, img, img0, self.cap
+
+    def new_video(self, path):
+        self.frame = 0
+        self.cap = cv2.VideoCapture(path)
+        self.nframes = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    def __len__(self):
+        return self.nf  # number of files
+
 def reset_folder():
         try:
             shutil.rmtree(car_path)
@@ -38,7 +172,28 @@ def reset_folder():
         Path(car_path).mkdir(parents=False, exist_ok=True)
         Path(no_car_path).mkdir(parents=False, exist_ok=True)
 
+def remove_borders(input_img):  # uses code from https://stackoverflow.com/a/48399309
+    
+    if np.array_equal(input_img[0][0], [244, 246, 247]) and np.array_equal(input_img[-1][-1], [244, 246, 247]):
+        ## (1) Convert to gray, and threshold
+        gray = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+        th, threshed = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
 
+        ## (2) Morph-op to remove noise
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11,11))
+        morphed = cv2.morphologyEx(threshed, cv2.MORPH_CLOSE, kernel)
+
+        ## (3) Find the max-area contour
+        cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        cnt = sorted(cnts, key=cv2.contourArea)[-1]
+
+        ## (4) Crop and save it
+        x,y,w,h = cv2.boundingRect(cnt)
+        dst = input_img[y:y+h, x:x+w]
+        return dst
+    else:
+        return input_img
+    
 def contains_car(input_image):
 
     source, weights, imgsz = input_image, "yolov7.pt", 640
@@ -54,21 +209,12 @@ def contains_car(input_image):
     just_return = False
     car_found = False   
 
-    #delete_old_dirs = False
-
-
-    #if delete_old_dirs:  
-        
-
     Path(car_path).mkdir(parents=False, exist_ok=True)
     Path(no_car_path).mkdir(parents=False, exist_ok=True)
 
    
     warnings.filterwarnings("ignore", category=DeprecationWarning) 
     warnings.filterwarnings("ignore", category=UserWarning) 
-
-    # Directories
-    #save_dir = ""
 
     # Initialize
     device = select_device('0')
@@ -82,7 +228,7 @@ def contains_car(input_image):
     if half:
         model.half()  # to FP16
 
-    dataset = LoadImages(source, img_size=imgsz, stride=stride)
+    dataset = NewLoadImages(source, img_size=imgsz, stride=stride)
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
@@ -98,13 +244,12 @@ def contains_car(input_image):
 
     t0 = time.time()
     for path, img, im0s, vid_cap in dataset:
+
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
-
-        
 
         # Warmup
         if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
@@ -131,8 +276,6 @@ def contains_car(input_image):
             else:
                 p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
 
-
-            
             txt_path = txt_name  
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
@@ -152,9 +295,20 @@ def contains_car(input_image):
                     if save_txt:
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                    
+                    box_on_edge = False
+                    upper_boundary = 0.995
+                    lower_boundary = 0.005
+
+                    if xywh[0] + xywh[2]/2 > upper_boundary or xywh[0] - xywh[2]/2 <lower_boundary:  # could be clever and do some abs() stuff here
+                        box_on_edge = True
+
+                    if xywh[1] + xywh[3]/2 > upper_boundary or xywh[1] - xywh[3]/2 <lower_boundary:
+                        box_on_edge = True
+                    
                     class_num = int(cls)
-                    if class_num == 2:
-                        #print(f"{class_num}, width = {xywh[2]:.2f} height = {xywh[3]:.2f}, area = {xywh[2]*xywh[3]:.2f}")
+                    bb_area = xywh[2]*xywh[3]
+                    if class_num == 2 and bb_area>0.1 and bb_area<0.9 and not box_on_edge:
                         car_found = True
 
                     if save_img or view_img:  # Add bbox to image
@@ -164,32 +318,19 @@ def contains_car(input_image):
             # Print time (inference + NMS)
             #print(f'\n{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS\n')
 
-            
             if just_return:
                 return True if car_found else False
             
-
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-
             # Save results (image with detections)
             if save_img:
                 prefix = car_path if car_found else no_car_path
                 save_path = f"img{len(os.listdir(car_path))}.jpg" if car_found else f"img{len(os.listdir(no_car_path))}.jpg"
                 cv2.imwrite(os.path.join(prefix,save_path), im0)
                 #print(f"The image with the result is saved in: {save_path}\n")
-                
-
-    '''if save_txt or save_img:
-        s = "" if save_txt else ''
-        #print(f"Results saved to {save_dir}{s}")'''
 
     print(f'Done. ({time.time() - t0:.3f}s)')
 
     return True if car_found else False
-
 
 reset_folder()
 
@@ -197,6 +338,5 @@ pics = os.listdir('images')
 
 for current_image in pics:
     contains_car(os.path.join('images',current_image))
-
 
 #contains_car("0.png")
